26a7a281b9c63072139816ac848857b612a60f42
MartiVilas <marti.vilas14@gmail.com>
2024-11-27 20:44:43 +0100

    finish

--- EA's/M2NF2EA5_VilasMarti.sql
+++ EA's/M2NF2EA5_VilasMarti.sql
@@ -112,3 +112,101 @@ CREATE TABLE IF NOT EXISTS Alumne_Assignatura (
     FOREIGN KEY (DNIAlumne) REFERENCES Alumne(DNI),
     FOREIGN KEY (CodiAssignatura) REFERENCES Assignatures(CodiAssignatura)
 );
+
+-- Tabla Aules
+INSERT INTO Aules (NumeroAula, MetresQuadrats) VALUES (106, '55m2');
+INSERT INTO Aules (NumeroAula, MetresQuadrats) VALUES (107, '65m2');
+INSERT INTO Aules (NumeroAula, MetresQuadrats) VALUES (108, '75m2');
+INSERT INTO Aules (NumeroAula, MetresQuadrats) VALUES (109, '85m2');
+INSERT INTO Aules (NumeroAula, MetresQuadrats) VALUES (110, '90m2');
+
+-- Tabla Moduls
+INSERT INTO Moduls (CodiModul, NomModul, NumeroAula) VALUES (6, 'Història', 106);
+INSERT INTO Moduls (CodiModul, NomModul, NumeroAula) VALUES (7, 'Geografia', 107);
+INSERT INTO Moduls (CodiModul, NomModul, NumeroAula) VALUES (8, 'Anglès', 108);
+INSERT INTO Moduls (CodiModul, NomModul, NumeroAula) VALUES (9, 'Música', 109);
+INSERT INTO Moduls (CodiModul, NomModul, NumeroAula) VALUES (10, 'Arts Plàstiques', 110);
+
+-- Tabla Assignatures
+INSERT INTO Assignatures (CodiAssignatura, NomAssignatura) VALUES (106, 'Història Contemporània');
+INSERT INTO Assignatures (CodiAssignatura, NomAssignatura) VALUES (107, 'Geopolítica');
+INSERT INTO Assignatures (CodiAssignatura, NomAssignatura) VALUES (108, 'Anglès Avançat');
+INSERT INTO Assignatures (CodiAssignatura, NomAssignatura) VALUES (109, 'Composició Musical');
+INSERT INTO Assignatures (CodiAssignatura, NomAssignatura) VALUES (110, 'Dibuix Tècnic');
+
+-- Tabla Persona
+INSERT INTO Persona (DNI, Nom, Cognom1, Cognom2, Adreca, Telefon) VALUES ('34567890C', 'Anna', 'Torres', 'Sánchez', 'Carrer Vell 3', '600345678');
+INSERT INTO Persona (DNI, Nom, Cognom1, Cognom2, Adreca, Telefon) VALUES ('45678901D', 'Pere', 'Ramírez', 'Lopez', 'Carrer Estret 4', '600456789');
+INSERT INTO Persona (DNI, Nom, Cognom1, Cognom2, Adreca, Telefon) VALUES ('56789012E', 'Clara', 'Soler', 'Martí', 'Carrer Llarg 5', '600567890');
+INSERT INTO Persona (DNI, Nom, Cognom1, Cognom2, Adreca, Telefon) VALUES ('67890123F', 'Jordi', 'Ferrer', 'Pérez', 'Carrer Ample 6', '600678901');
+INSERT INTO Persona (DNI, Nom, Cognom1, Cognom2, Adreca, Telefon) VALUES ('78901234G', 'Carme', 'Vila', 'Rodríguez', 'Carrer Curt 7', '600789012');
+
+-- Tabla Professor
+INSERT INTO Professor (DNI, Especialitat, Assignatures) VALUES ('12345678A', 'Matemàtiques', 'Àlgebra Lineal');
+INSERT INTO Professor (DNI, Especialitat, Assignatures) VALUES ('23456789B', 'Física', 'Electromagnetisme');
+INSERT INTO Professor (DNI, Especialitat, Assignatures) VALUES ('34567890C', 'Química', 'Reaccions Químiques');
+INSERT INTO Professor (DNI, Especialitat, Assignatures) VALUES ('45678901D', 'Biologia', 'Genètica Molecular');
+INSERT INTO Professor (DNI, Especialitat, Assignatures) VALUES ('56789012E', 'Informàtica', 'Programació Avançada');
+
+-- Tabla Alumne
+INSERT INTO Alumne (DNI, DNIAlume, DataNaixement, AssignaturesMatriculades) VALUES ('78901234G', NULL, '2005-03-15', 'Àlgebra Lineal');
+INSERT INTO Alumne (DNI, DNIAlume, DataNaixement, AssignaturesMatriculades) VALUES ('67890123F', NULL, '2004-05-20', 'Electromagnetisme');
+INSERT INTO Alumne (DNI, DNIAlume, DataNaixement, AssignaturesMatriculades) VALUES ('56789012E', NULL, '2003-07-10', 'Reaccions Químiques');
+INSERT INTO Alumne (DNI, DNIAlume, DataNaixement, AssignaturesMatriculades) VALUES ('45678901D', '78901234G', '2002-09-25', 'Genètica Molecular');
+INSERT INTO Alumne (DNI, DNIAlume, DataNaixement, AssignaturesMatriculades) VALUES ('34567890C', NULL, '2001-12-30', 'Programació Avançada');
+
+-- Tabla Modul_Assignatura
+INSERT INTO Modul_Assignatura (CodiModul, CodiAssignatura) VALUES (1, 101);
+INSERT INTO Modul_Assignatura (CodiModul, CodiAssignatura) VALUES (2, 102);
+INSERT INTO Modul_Assignatura (CodiModul, CodiAssignatura) VALUES (3, 103);
+INSERT INTO Modul_Assignatura (CodiModul, CodiAssignatura) VALUES (4, 104);
+INSERT INTO Modul_Assignatura (CodiModul, CodiAssignatura) VALUES (5, 105);
+
+-- Tabla Modul_Aula
+INSERT INTO Modul_Aula (CodiModul, NumeroAula) VALUES (1, 101);
+INSERT INTO Modul_Aula (CodiModul, NumeroAula) VALUES (2, 102);
+INSERT INTO Modul_Aula (CodiModul, NumeroAula) VALUES (3, 103);
+INSERT INTO Modul_Aula (CodiModul, NumeroAula) VALUES (4, 104);
+INSERT INTO Modul_Aula (CodiModul, NumeroAula) VALUES (5, 105);
+
+-- Tabla Professor_Assignatura
+INSERT INTO Professor_Assignatura (DNIProfessor, CodiAssignatura) VALUES ('12345678A', 101);
+INSERT INTO Professor_Assignatura (DNIProfessor, CodiAssignatura) VALUES ('23456789B', 102);
+INSERT INTO Professor_Assignatura (DNIProfessor, CodiAssignatura) VALUES ('34567890C', 103);
+INSERT INTO Professor_Assignatura (DNIProfessor, CodiAssignatura) VALUES ('45678901D', 104);
+INSERT INTO Professor_Assignatura (DNIProfessor, CodiAssignatura) VALUES ('56789012E', 105);
+
+-- Tabla Alumne_Assignatura
+INSERT INTO Alumne_Assignatura (DNIAlumne, CodiAssignatura) VALUES ('78901234G', 101);
+INSERT INTO Alumne_Assignatura (DNIAlumne, CodiAssignatura) VALUES ('67890123F', 102);
+INSERT INTO Alumne_Assignatura (DNIAlumne, CodiAssignatura) VALUES ('56789012E', 103);
+INSERT INTO Alumne_Assignatura (DNIAlumne, CodiAssignatura) VALUES ('45678901D', 104);
+INSERT INTO Alumne_Assignatura (DNIAlumne, CodiAssignatura) VALUES ('34567890C', 105);
+
+
+/* ALTER TABLES */
+ALTER TABLE Persona ADD Email VARCHAR(100);
+
+ALTER TABLE Persona MODIFY Telefon VARCHAR(20);
+
+ALTER TABLE Persona ADD CONSTRAINT UK_Persona_Telefon UNIQUE (Telefon);
+
+ALTER TABLE Persona DROP COLUMN Cognom2;
+
+/* UPDATES */
+UPDATE Persona SET Nom = 'María' WHERE DNI = '12345678A';
+
+UPDATE Aules SET MetresQuadrats = '60m2' WHERE CAST(SUBSTR(MetresQuadrats, 1, LENGTH(MetresQuadrats) - 2) AS INT) < 60;
+
+UPDATE Moduls SET NumeroAula = (SELECT NumeroAula FROM Aules WHERE NumeroAula = 110) WHERE CodiModul = 5;
+
+UPDATE Persona SET Adreca = 'Carrer Nou 10', Telefon = '611223344' WHERE DNI = '23456789B';
+
+/* DELETES */
+DELETE FROM Alumne WHERE DNI = '78901234G';
+
+DELETE FROM Moduls WHERE CodiModul = 5;
+
+DELETE FROM Assignatures;
+
+DELETE FROM Alumne WHERE YEAR(DataNaixement) < 2004;
